ee465727000f7473d04e357ff545126972aada19
JRB <jrbatemanjr@gmail.com>
2023-05-10 20:35:01 -0500

    Bateman update

--- app/src/main/java/com/thies/photogallery/PhotoRepository.kt
+++ app/src/main/java/com/thies/photogallery/PhotoRepository.kt
@@ -3,20 +3,26 @@ package com.thies.photogallery
 import com.thies.photogallery.api.FlickrApi
 import com.thies.photogallery.api.GalleryItem
 import retrofit2.Retrofit
-import retrofit2.converter.moshi.MoshiConverterFactory
+//import retrofit2.converter.moshi.MoshiConverterFactory
+import retrofit2.converter.scalars.ScalarsConverterFactory
 import retrofit2.create
 
 class PhotoRepository {
+
     private val flickrApi: FlickrApi
 
     init {
         // building retrofit object
         val retrofit: Retrofit = Retrofit.Builder()
             .baseUrl("https://api.flickr.com/")
-            .addConverterFactory(MoshiConverterFactory.create())
+            .addConverterFactory(ScalarsConverterFactory.create())
+           // .addConverterFactory(MoshiConverterFactory.create())
             .build()
-        flickrApi = retrofit.create()
+       flickrApi = retrofit.create()
+
+
     }
 
-    suspend fun fetchPhotos(): List<GalleryItem> = flickrApi.fetchPhotos().photos.galleryItems
+    suspend fun fetchPhotos() = flickrApi.fetchPhotos()
+   // suspend fun fetchPhotos(): List<GalleryItem> = flickrApi.fetchPhotos().photos.galleryItems
 }
--- app/src/main/java/com/thies/photogallery/api/FlickrApi.kt
+++ app/src/main/java/com/thies/photogallery/api/FlickrApi.kt
@@ -5,12 +5,15 @@ import retrofit2.http.GET
 private const val API_KEY = "6acbc8486ffcf810f03d38a1907d6854"
 
 interface FlickrApi {
-    @GET(
+  @GET(
         "services/rest/?method=flickr.interestingness.getList" +
             "&api_key=$API_KEY" +
             "&format=json" +
             "&nojsoncallback=1" +
             "&extras=url_s"
     )
-    suspend fun fetchPhotos(): FlickrResponse
+
+ suspend fun fetchPhotos(): String
+
+ //suspend fun fetchPhotos(): FlickrResponse
 }
