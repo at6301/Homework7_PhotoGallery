20794f407c118f4c79b273f1df1903c36e4c9b80
Alex Thies <at6301@live.missouristate.edu>
2023-05-11 17:09:41 -0500

    Finished code

---
+++ app/src/main/java/com/thies/photogallery/PhotoGalleryViewModel.kt
@@ -0,0 +1,34 @@
+package com.thies.photogallery
+
+import android.util.Log
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.thies.photogallery.api.GalleryItem
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.launch
+
+private const val TAG = "PhotoGalleryViewModel"
+
+class PhotoGalleryViewModel : ViewModel() {
+
+    private val photoRepository = PhotoRepository()
+
+    private val _galleryItems: MutableStateFlow<List<GalleryItem>> =
+        MutableStateFlow(emptyList())
+
+    val galleryItems: StateFlow<List<GalleryItem>>
+        get() = _galleryItems.asStateFlow()
+
+    init {
+        viewModelScope.launch {
+            try {
+                val items = photoRepository.fetchPhotos()
+                Log.d(TAG, "Items received: $items")
+            } catch (ex: Exception) {
+                Log.d(TAG, "Failed to fetch gallery items", ex)
+            }
+        }
+    }
+}
